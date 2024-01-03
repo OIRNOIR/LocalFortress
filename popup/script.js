58a89736109fefddbdd4cb43aa9c017c8501f511
OIRNOIR <70721372+OIRNOIR@users.noreply.github.com>
2024-01-03 10:03:57 -0800

    Popup

--- popup/script.js
+++ popup/script.js
@@ -17,31 +17,53 @@ async function main() {
 	})
 	const currentTabs = await browser.tabs.query({active: true});
 	console.log(currentTabs);
-	if (whitelist.indexOf(new URL(currentTabs[0].url).hostname) != -1) {
-		thissite.innerText = "Remove This Site From Whitelist"
-	}
-	thissite.addEventListener("click", async () => {
+	if (new URL(currentTabs[0].url).hostname == "") {
+		thissite.style.display = "none";
+	} else {
 		if (whitelist.indexOf(new URL(currentTabs[0].url).hostname) != -1) {
-			whitelist.splice(whitelist.indexOf(new URL(currentTabs[0].url).hostname), 1);
-			thissite.innerText = "Whitelist This Site"
-		} else {
-			whitelist.push(new URL(currentTabs[0].url).hostname);
 			thissite.innerText = "Remove This Site From Whitelist"
+			thissite.classList.remove("primary");
+			thissite.classList.add("danger");
 		}
-		await browser.storage.sync.set({"whitelist": whitelist});
-	})
+		thissite.addEventListener("click", async () => {
+			if (whitelist.indexOf(new URL(currentTabs[0].url).hostname) != -1) {
+				whitelist.splice(whitelist.indexOf(new URL(currentTabs[0].url).hostname), 1);
+				thissite.innerText = "Whitelist This Site"
+				thissite.classList.remove("danger");
+				thissite.classList.add("primary");
+			} else {
+				whitelist.push(new URL(currentTabs[0].url).hostname);
+				thissite.innerText = "Remove This Site From Whitelist"
+				thissite.classList.remove("primary");
+				thissite.classList.add("danger");
+			}
+			await browser.storage.sync.set({"whitelist": whitelist});
+		})
+	}
 	function refreshWhitelistView() {
 		whiltelistView.innerHTML = "";
-		for (const site of whitelist) {
-			const el = document.createElement("button")
-			el.textContent = site;
-			el.addEventListener("click", async () => {
-				whitelist.splice(whitelist.indexOf(site), 1);
-				if (new URL(currentTabs[0].url).hostname == site) thissite.innerText = "Whitelist This Site";
-				await browser.storage.sync.set({"whitelist": whitelist});
-				refreshWhitelistView();
-			})
-			whiltelistView.appendChild(el);
+		if (whitelist.length == 0) {
+			const text = document.createElement("i");
+			text.innerText = "No whitelisted sites";
+			text.style.fontSize = "10px";
+			whiltelistView.appendChild(text);
+		} else {
+			for (const site of whitelist) {
+				const el = document.createElement("button")
+				el.textContent = site;
+				el.classList.add("secondary");
+				el.addEventListener("click", async () => {
+					whitelist.splice(whitelist.indexOf(site), 1);
+					if (new URL(currentTabs[0].url).hostname == site) {
+						thissite.innerText = "Whitelist This Site";
+						thissite.classList.remove("danger");
+						thissite.classList.add("primary");
+					}
+					await browser.storage.sync.set({"whitelist": whitelist});
+					refreshWhitelistView();
+				});
+				whiltelistView.appendChild(el);
+			}
 		}
 	}
 	refreshWhitelistView();
