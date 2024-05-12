c7fe59b1c5753fac812044c44df66f98171531ee
OIRNOIR <70721372+OIRNOIR@users.noreply.github.com>
2024-05-12 12:08:07 -0700

    Move to biome, fix a small bug

--- index.js
+++ index.js
@@ -14,93 +14,106 @@ async function asyncActions() {
 	const resultOne = await browser.storage.sync.get("whitelist");
 	whitelist = resultOne.whitelist;
 	if (whitelist == null) {
-		whitelist = []
-		await browser.storage.sync.set({"whitelist": whitelist});
+		whitelist = [];
+		await browser.storage.sync.set({ whitelist: whitelist });
 	}
 }
 
 /**
- * @param {browser.webRequest._OnBeforeRequestDetails} details 
+ * @param {browser.webRequest._OnBeforeRequestDetails} details
  */
 function determineOriginPermission(details) {
 	if (details.documentUrl == null) return true; // This is the top-level document
-	for (const origin of [details.documentUrl, ...details.frameAncestors.map(a => a.url)]) {
-		if (["0.0.0.0", "localhost", "::1"].indexOf(new URL(origin).hostname) == -1 && !(new URL(origin).hostname.match(/127\.\d{1,3}\.\d{1,3}\.\d{1,3}/g)?.length > 0) && whitelist.indexOf(new URL(origin).hostname) == -1) return false;
+	for (const origin of [details.documentUrl, ...details.frameAncestors.map((a) => a.url)]) {
+		if (
+			["0.0.0.0", "localhost", "::1"].indexOf(new URL(origin).hostname) == -1 &&
+			!(new URL(origin).hostname.match(/127\.\d{1,3}\.\d{1,3}\.\d{1,3}/g)?.length > 0) &&
+			whitelist.indexOf(new URL(origin).hostname) == -1
+		)
+			return false;
 	}
 	return true;
 }
 
-browser.webRequest.onBeforeRequest.addListener(details => {
-	if (["0.0.0.0", "localhost", "::1"].indexOf(new URL(details.url).hostname) != -1 || new URL(details.url).hostname.match(/127\.\d{1,3}\.\d{1,3}\.\d{1,3}/g)?.length > 0) {
-		if (determineOriginPermission(details) == false) {
-			if (details.tabId != -1) {
-				if (requestCounts.has(details.tabId)) {
-					requestCounts.set(details.tabId, requestCounts.get(details.tabId) + 1);
-				} else {
-					requestCounts.set(details.tabId, 1);
+browser.webRequest.onBeforeRequest.addListener(
+	(details) => {
+		if (
+			["0.0.0.0", "localhost", "::1"].indexOf(new URL(details.url).hostname) != -1 ||
+			new URL(details.url).hostname.match(/127\.\d{1,3}\.\d{1,3}\.\d{1,3}/g)?.length > 0
+		) {
+			if (determineOriginPermission(details) == false) {
+				if (details.tabId != -1) {
+					if (requestCounts.has(details.tabId)) {
+						requestCounts.set(details.tabId, requestCounts.get(details.tabId) + 1);
+					} else {
+						requestCounts.set(details.tabId, 1);
+					}
 				}
-			}
-			browser.action.setBadgeText({text: requestCounts.has(details.tabId) ? String(requestCounts.get(details.tabId)) : null, tabId: details.tabId});
-			browser.action.setBadgeBackgroundColor({color: "#5b5b66"});
-			return {
-				cancel: true
+				browser.action.setBadgeText({
+					text: requestCounts.has(details.tabId) ? String(requestCounts.get(details.tabId)) : null,
+					tabId: details.tabId,
+				});
+				browser.action.setBadgeBackgroundColor({ color: "#5b5b66" });
+				return {
+					cancel: true,
+				};
 			}
 		}
-	}
-	return {
-		cancel: false
-	}
-}, {
-	urls: ["<all_urls>", "ws://*/*", "wss://*/*"],
-	types: [
-		"beacon",
-		"csp_report",
-		"font",
-		"image",
-		"imageset",
-		"media",
-		"object",
-		"script",
-		"speculative",
-		"stylesheet",
-		"sub_frame",
-		"web_manifest",
-		"websocket",
-		"xml_dtd",
-		"xmlhttprequest",
-		"xslt",
-		"other"
-	]
-}, [
-	"blocking"
-]);
+		return {
+			cancel: false,
+		};
+	},
+	{
+		urls: ["<all_urls>", "ws://*/*", "wss://*/*"],
+		types: [
+			"beacon",
+			"csp_report",
+			"font",
+			"image",
+			"imageset",
+			"media",
+			"object",
+			"script",
+			"speculative",
+			"stylesheet",
+			"sub_frame",
+			"web_manifest",
+			"websocket",
+			"xml_dtd",
+			"xmlhttprequest",
+			"xslt",
+			"other",
+		],
+	},
+	["blocking"],
+);
 
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 	if (!permissions) return;
 	if (changeInfo.status == "loading") requestCounts.delete(tabId);
-	await browser.action.setBadgeText({text: ""});
-	await browser.action.setBadgeBackgroundColor({color: "#5b5b66"});
-})
+	await browser.action.setBadgeText({ text: "" });
+	await browser.action.setBadgeBackgroundColor({ color: "#5b5b66" });
+});
 
 browser.storage.onChanged.addListener((changes, areaName) => {
 	if (areaName == "sync" && changes.whitelist != null && changes.whitelist.newValue != null) {
 		whitelist = changes.whitelist.newValue;
 	}
-})
+});
 
 void asyncActions();
 
-browser.permissions.contains({origins: ["<all_urls>"]}).then(async status => {
+browser.permissions.contains({ origins: ["<all_urls>"] }).then(async (status) => {
 	if (status) {
 		permissions = true;
 	} else {
-		await browser.action.setBadgeText({text: "!"});
-		await browser.action.setBadgeBackgroundColor({color: "#ffee00"});
+		await browser.action.setBadgeText({ text: "!" });
+		await browser.action.setBadgeBackgroundColor({ color: "#ffee00" });
 	}
 });
 
-browser.permissions.onAdded.addListener(permissions => {
-	if (permissions.origins.includes("<all_urls>")) {
+browser.permissions.onAdded.addListener((perms) => {
+	if (perms.origins.includes("<all_urls>")) {
 		permissions = true;
 	}
-})
+});
--- popup/script.js
+++ popup/script.js
@@ -14,30 +14,30 @@ async function main() {
 			whitelist = changes.whitelist.newValue;
 			refreshWhitelistView();
 		}
-	})
-	const currentTabs = await browser.tabs.query({active: true});
+	});
+	const currentTabs = await browser.tabs.query({ active: true });
 	if (new URL(currentTabs[0].url).hostname == "") {
 		thissite.style.display = "none";
 	} else {
 		if (whitelist.indexOf(new URL(currentTabs[0].url).hostname) != -1) {
-			thissite.innerText = "Remove This Site From Whitelist"
+			thissite.innerText = "Remove This Site From Whitelist";
 			thissite.classList.remove("primary");
 			thissite.classList.add("danger");
 		}
 		thissite.addEventListener("click", async () => {
 			if (whitelist.indexOf(new URL(currentTabs[0].url).hostname) != -1) {
 				whitelist.splice(whitelist.indexOf(new URL(currentTabs[0].url).hostname), 1);
-				thissite.innerText = "Whitelist This Site"
+				thissite.innerText = "Whitelist This Site";
 				thissite.classList.remove("danger");
 				thissite.classList.add("primary");
 			} else {
 				whitelist.push(new URL(currentTabs[0].url).hostname);
-				thissite.innerText = "Remove This Site From Whitelist"
+				thissite.innerText = "Remove This Site From Whitelist";
 				thissite.classList.remove("primary");
 				thissite.classList.add("danger");
 			}
-			await browser.storage.sync.set({"whitelist": whitelist});
-		})
+			await browser.storage.sync.set({ whitelist: whitelist });
+		});
 	}
 	function refreshWhitelistView() {
 		whiltelistView.innerHTML = "";
@@ -48,7 +48,7 @@ async function main() {
 			whiltelistView.appendChild(text);
 		} else {
 			for (const site of whitelist) {
-				const el = document.createElement("button")
+				const el = document.createElement("button");
 				el.textContent = site;
 				el.classList.add("secondary");
 				el.addEventListener("click", async () => {
@@ -58,7 +58,7 @@ async function main() {
 						thissite.classList.remove("danger");
 						thissite.classList.add("primary");
 					}
-					await browser.storage.sync.set({"whitelist": whitelist});
+					await browser.storage.sync.set({ whitelist: whitelist });
 					refreshWhitelistView();
 				});
 				whiltelistView.appendChild(el);
@@ -68,19 +68,19 @@ async function main() {
 	refreshWhitelistView();
 }
 
-browser.permissions.contains({origins: ["<all_urls>"]}).then(async status => {
+browser.permissions.contains({ origins: ["<all_urls>"] }).then(async (status) => {
 	if (status) {
 		await main();
 	} else {
 		document.getElementById("content").style.display = "none";
 		document.getElementById("permissions-needed").style.display = "block";
 		document.getElementById("reqperms").addEventListener("click", async () => {
-			const result = await browser.permissions.request({origins: ["<all_urls>"]});
+			const result = await browser.permissions.request({ origins: ["<all_urls>"] });
 			if (result) {
 				document.getElementById("content").style.display = "block";
 				document.getElementById("permissions-needed").style.display = "none";
-				await browser.action.setBadgeText({text: ""});
-				await browser.action.setBadgeBackgroundColor({color: "#5b5b66"});
+				await browser.action.setBadgeText({ text: "" });
+				await browser.action.setBadgeBackgroundColor({ color: "#5b5b66" });
 				await main();
 			}
 		});
